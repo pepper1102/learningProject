ddf1a6e5fb319ef254e0e4174c535ed3c5765ea8
pepper1102 <dr11211037@gmail.com>
2025-12-15 20:54:42 +0900

    feat: リスト表示削除スタイル適用機能の実装 (課題1-3 ~ 1-5)

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,49 @@
 import { ref } from 'vue'
 
 const title = ref('Learning Project')
-const count = ref(0)
+const count = ref(0);
+const items = ref([
+  { title: '学習課題1', isDone: false },
+  { title: '学習課題2', isDone: false },
+  { title: '学習課題3', isDone: false }]);
+const newItem = ref('');
+
 
 const increment = () => {
-    count.value++ // refの値を変えるときは .value が必要
+  count.value++ // refの値を変えるときは .value が必要
+}
+
+const addItem = () => {
+  if (newItem.value.trim() !== '') {
+    items.value.push({
+      title: newItem.value,
+      isDone: false
+    });
+    newItem.value = '';
+  }
+}
+const deleteItem = (index) => {
+  items.value.splice(index, 1);
 }
 </script>
+
 
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
     <p>簡単なページを作成しました。</p>
     <p>カウント: {{ count }}</p>
-    <button @click="increment">カウントアップ</button>
-    <p>
     <input type="text" v-model="title">
-    </p>
-
+    <ul>
+      <li v-for="(item, index) in items" :key="item">
+        <span :class="{ done: item.isDone }" @click="item.isDone = !item.isDone">
+          {{ item.title }}
+        </span>
+        <button @click="deleteItem(index)">削除</button>
+      </li>
+    </ul>
+    <input type="text" v-model="newItem">
+    <button @click="addItem">追加</button>
   </div>
 </template>
 
@@ -27,5 +53,10 @@ const increment = () => {
   font-family: sans-serif;
   text-align: center;
   margin-top: 50px;
+}
+
+.done {
+  text-decoration: line-through;
+  color: gray;
 }
 </style>
