52e332656224e15d1402f76a09522ec190179111
pepper1102 <103540738+pepper1102@users.noreply.github.com>
2025-12-15 19:44:54 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Learning Project')
+const count = ref(0)
+
+const increment = () => {
+    count.value++ // refの値を変えるときは .value が必要
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <p>簡単なページを作成しました。</p>
+    <p>カウント: {{ count }}</p>
+    <button @click="increment">カウントアップ</button>
+    <p>
+    <input type="text" v-model="title">
+    </p>
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
